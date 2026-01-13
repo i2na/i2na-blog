dab62ba0bdbab6938deac5130e81392d0db40097
YenaLey <yena.e121@gmail.com>
2026-01-13 14:51:48 +0900

    chore: client 기본 포트 5174로 변경

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     ],
     envDir: "../",
     server: {
-        port: 5173,
+        port: 5174,
         proxy: {
             "/api": {
                 target: "http://localhost:3000",
